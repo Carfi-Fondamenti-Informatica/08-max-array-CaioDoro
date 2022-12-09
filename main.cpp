875843b8421e8ffecfeafbe06eb944a0edc5d462
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-12-09 18:59:27 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
-}
+int main (){
+    int g;
+    cin >> g;
+    float arr[g];
+    float n;
+    int i;
+
+    for(i=0; i<g; i++){
+        cin>>arr[i];
+    n = arr[0];}
+    for(i=1; i<g; i++)
+    {
+        if(n<arr[i])
+            n = arr[i];
+    }
+    cout << n << endl;
+    return 0;}
